97cac3e9ff1e485d6d0aa541309c004333b335e2
verj <mona.liza2000kpy@gmail.com>
2025-03-24 21:25:02 +0500

    lb2

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -9,15 +9,14 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username', 191)->unique();
+            $table->string('email',191)->unique();
             $table->string('password');
-            $table->rememberToken();
+            $table->date('birthday');
             $table->timestamps();
         });
 
